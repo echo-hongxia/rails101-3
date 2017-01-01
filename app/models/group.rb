6133663f4044cbec3b364345fd30ca9b7c5986e8
apple <apple@appledeMacBook-Pro.local>
2017-01-01 16:08:48 +0800

    add user_id to group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
-class Group < ActiveRecord::Base
+class Group < ApplicationRecord
   validates :title,presence: true
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170101073410) do
+ActiveRecord::Schema.define(version: 20170101080147) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "use_id"
   end
 
   create_table "users", force: :cascade do |t|
