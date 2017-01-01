76570086a5629942b827e2863f1dca95b65f8c5b
apple <apple@appledeMacBook-Pro.local>
2017-01-02 05:23:53 +0800

    add  delete and edit post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,28 @@
 class PostsController < ApplicationController
   before_filter :authenticate_user!, :only =>[:new ,:create]
 
-  
+  def edit
+    @post = Post.find(params[:id])
+   @group = Group.find(params[:group_id])
+  end
 
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path,notice: "Updated Success!"
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path,alert: "Post Deleted!"
+
+
+  end
 
 
   def new
